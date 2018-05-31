2c45b796236c7ef1dc16e2cc742d5a0b8f5f6dd9
Derek Haus <rumbleincybertron@gmail.com>
2018-05-31 22:24:48 +0900

    Converting Blueprint to C++

--- Stealth/Source/Stealth/ChooseNextWaypoint.cpp
+++ Stealth/Source/Stealth/ChooseNextWaypoint.cpp
@@ -1,12 +1,33 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWaypoint.h"
+#include "AIController.h"
+#include "PatrollingGuard.h" // TODO Remove coupling
 #include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
 {
-	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
-	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
-	UE_LOG(LogTemp, Warning, TEXT("Waypoint index: %i"), Index)
+	// TODO Protect against empty patrol routes
+
+	// Get Patrol Points
+	auto AIController = OwnerComp.GetAIOwner();
+	auto ControlledPawn = AIController->GetPawn();
+	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
+	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+
+	// Set Next Waypoint
+	if (PatrolPoints.Num() > 0)
+	{
+		auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+		auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+		BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+		
+		// Cycle the index
+		auto NextIndex = (Index + 1) % PatrolPoints.Num();
+		BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+	}
+
+
+
 	return EBTNodeResult::Succeeded;
 }
