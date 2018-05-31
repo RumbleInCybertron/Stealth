1953585a3e7dbd2db67a17630f4ba9e12592ca79
Derek Haus <rumbleincybertron@gmail.com>
2018-05-31 18:13:57 +0900

    C++ AI Behavior Tree Tasks
    TODO Remove mistakingly made class.

---
+++ Stealth/Source/Stealth/MyBTTaskNode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyBTTaskNode.h"
+
+EBTNodeResult::Type UMyBTTaskNode::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
+{
+	return EBTNodeResult::Succeeded;
+}
