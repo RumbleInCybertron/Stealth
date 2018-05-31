b166e92db951cb8b2927723f9d651da1e56cf862
Derek Haus <rumbleincybertron@gmail.com>
2018-05-31 18:28:07 +0900

    Replaced Wrong Class
    Replaced MyBTTaskNode.cpp/.h files with ChooseNextWaypoint.cpp/.h files

--- Stealth/Source/Stealth/ChooseNextWaypoint.cpp
+++ Stealth/Source/Stealth/ChooseNextWaypoint.cpp
@@ -4,5 +4,6 @@
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
 {
+	UE_LOG(LogTemp, Warning, TEXT("AI in C++ - yay!"))
 	return EBTNodeResult::Succeeded;
 }
--- Stealth/Source/Stealth/MyBTTaskNode.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "MyBTTaskNode.h"
-
-EBTNodeResult::Type UMyBTTaskNode::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
-{
-	return EBTNodeResult::Succeeded;
-}
