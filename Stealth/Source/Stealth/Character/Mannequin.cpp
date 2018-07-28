46fdbd232794f4ca8ac1aad545e8d20bda417c85
Derek Haus <rumbleincybertron@gmail.com>
2018-07-28 20:20:16 +0900

    Reattached Gun
    TODO: NPC's gun is on the floor. Needs to be resolved.

--- Stealth/Source/Stealth/Character/Mannequin.cpp
+++ Stealth/Source/Stealth/Character/Mannequin.cpp
@@ -29,11 +29,6 @@ AMannequin::AMannequin()
 	Mesh1P->CastShadow = false;
 	Mesh1P->RelativeRotation = FRotator(1.9f, -19.19f, 5.2f);
 	Mesh1P->RelativeLocation = FVector(-0.5f, -4.4f, -155.7f);
-
-	if (InputComponent != NULL)
-	{
-		InputComponent->BindAction("Fire", IE_Pressed, this, &AMannequin::PullTrigger);
-	}
 }
 
 // Called when the game starts or when spawned
@@ -42,13 +37,29 @@ void AMannequin::BeginPlay()
 	// Call the base class  
 	Super::BeginPlay();
 
-	if (GunBlueprint == NULL) {
+	if (GunBlueprint == nullptr) {
 		UE_LOG(LogTemp, Warning, TEXT("Gun Blueprint missing"))
 			return;
 	}
+
 	Gun = GetWorld()->SpawnActor<AGun>(GunBlueprint);
-	Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
-	Gun->AnimInstance = GetMesh()->GetAnimInstance();	
+
+	if (IsPlayerControlled())
+	{
+		Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
+	}
+	else
+	{
+		Gun->AttachToComponent(GetMesh(), FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint_0"));
+	}
+
+	Gun->AnimInstance1P = Mesh1P->GetAnimInstance();
+	Gun->AnimInstance3P = GetMesh()->GetAnimInstance();	
+
+	if (InputComponent != nullptr)
+	{
+		InputComponent->BindAction("Fire", IE_Pressed, this, &AMannequin::PullTrigger);
+	}
 }
 
 // Called every frame
@@ -61,6 +72,18 @@ void AMannequin::Tick(float DeltaTime)
 void AMannequin::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void AMannequin::UnPossessed()
+{
+	Super::UnPossessed();
+	if (!ensure(Gun))
+	{
+		UE_LOG(LogTemp, Error, TEXT("Gun is not available"))
+		return;
+	}
+	UE_LOG(LogTemp, Error, TEXT("Gun re-attached"))
+	Gun->AttachToComponent(GetMesh(), FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
 }
 
 void AMannequin::PullTrigger()
