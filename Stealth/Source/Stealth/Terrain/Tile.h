4c3807c57163981732a4854501e18aaa3f29d9d4
Derek Haus <rumbleincybertron@gmail.com>
2018-08-03 20:05:17 +0900

    Structs to Simplify Function Arguments

--- Stealth/Source/Stealth/Terrain/Tile.h
+++ Stealth/Source/Stealth/Terrain/Tile.h
@@ -6,6 +6,18 @@
 #include "GameFramework/Actor.h"
 #include "Tile.generated.h"
 
+
+USTRUCT()
+struct FSpawnPosition
+{
+	GENERATED_USTRUCT_BODY()
+
+	FVector Location;
+	float Rotation;
+	float Scale;
+};
+
+
 class UActorPool;
 
 UCLASS()
@@ -45,9 +57,11 @@ public:
 private:
 	void PositionNavMeshBoundsVolume();
 
+	TArray<FSpawnPosition> RandomSpawnPositions(int32 MinSpawn, int32 MaxSpawn, float MinScale, float MaxScale, float Radius);
+
 	bool FindEmptyLocation(FVector& Outlocation, float Radius);
 
-	void PlaceActor(TSubclassOf<AActor> ToSpawn, FVector SpawnPoint, float Rotation, float Scale);
+	void PlaceActor(TSubclassOf<AActor> ToSpawn, const FSpawnPosition& SpawnPosition);
 
 	bool CanSpawnAtLocation(FVector Location, float Radius);
 	
