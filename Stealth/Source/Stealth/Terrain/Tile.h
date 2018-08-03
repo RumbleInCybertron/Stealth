8efca9d646e399d38cf315b15899b85b91e11a7a
Derek Haus <rumbleincybertron@gmail.com>
2018-08-03 21:39:52 +0900

    Fixed Double Spawning
    Used Template in Tile.cpp/.h to get rid of code that double spawned objects.

--- Stealth/Source/Stealth/Terrain/Tile.h
+++ Stealth/Source/Stealth/Terrain/Tile.h
@@ -64,9 +64,12 @@ private:
 
 	bool FindEmptyLocation(FVector& Outlocation, float Radius);
 
+	template<class T>
+	void RandomlyPlaceActors(TSubclassOf<T> ToSpawn, int32 MinSpawn = 1, int32 MaxSpawn = 1, float Radius = 500, float MinScale = 1, float MaxScale = 1);
+
 	void PlaceActor(TSubclassOf<AActor> ToSpawn, const FSpawnPosition& SpawnPosition);
 
-	void PlaceAIPawn(TSubclassOf<APawn> ToSpawn, FSpawnPosition SpawnPosition);
+	void PlaceActor(TSubclassOf<APawn> ToSpawn, FSpawnPosition SpawnPosition);
 
 	bool CanSpawnAtLocation(FVector Location, float Radius);
 	
